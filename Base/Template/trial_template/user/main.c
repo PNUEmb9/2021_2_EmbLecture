92e01b35caa63e33d482fae882716723878d6e57
JaeHeyK <jaeheyk@gmail.com>
2021-10-28 16:56:26 +0900

    [Week09] Update initial firmware code

--- Base/Template/trial_template/user/main.c
+++ Base/Template/trial_template/user/main.c
@@ -1,5 +1,4 @@
 #include "stm32f10x.h"
-#include "stm32f10x_exti.h"
 #include "stm32f10x_gpio.h"
 #include "stm32f10x_usart.h"
 #include "stm32f10x_rcc.h"
@@ -10,7 +9,7 @@
 void RCC_Configure(void);
 void GPIO_Configure(void);
 void EXTI_Configure(void);
-void USART1_Init(void);
+void USART_Init(void);
 void NVIC_Configure(void);
 
 void EXTI15_10_IRQHandler(void);
@@ -20,56 +19,33 @@ void EXTI2_IRQHandler(void);
 void Delay(void);
 
 void sendDataUART1(uint16_t data);
+void sendDataUART2(uint16_t data);
 
-char sw_state; // 기본 상태; 1234 방향으로 led 점멸 중
-char debounce_flag=0;
+char dataBufferFromPC;
+char flagPC = 0;
+
+char dataBufferFromBT;
+char flagBT = 0;
+
 
 //---------------------------------------------------------------------------------------------------
 
 void RCC_Configure(void) // stm32f10x_rcc.h 참고
 {
-	// TODO: Enable the APB2 peripheral clock using the function 'RCC_APB2PeriphClockCmd'
-	
-	/* UART TX/RX port clock enable */
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-	
-	/* JoyStick Up/Down port clock enable */
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-	
-	/* LED port clock enable */
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
-
 	/* USART1 clock enable */
     RCC_APB2PeriphClockCmd(RCC_APB2ENR_USART1EN, ENABLE);
 
-	
-	/* Alternate Function IO clock enable */
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+	/* USART2 clock enable */
+    RCC_APB2PeriphClockCmd(RCC_APB2ENR_USART2EN, ENABLE);
 }
 
 void GPIO_Configure(void) // stm32f10x_gpio.h 참고
 {
     GPIO_InitTypeDef GPIO_InitStructure;
 
-	// TODO: Initialize the GPIO pins using the structure 'GPIO_InitTypeDef' and the function 'GPIO_Init'
-	
-    /* JoyStick up, down pin setting */
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_2;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-    GPIO_Init(GPIOC, &GPIO_InitStructure);
-    
-    /* button pin setting */
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
-    
-    /* LED pin setting*/
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_7;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
-	
-    /* UART pin setting */
+
+
+    /* UART between PC pin setting */
     //TX
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
@@ -81,50 +57,29 @@ void GPIO_Configure(void) // stm32f10x_gpio.h 참고
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 	
+    /* UART between bluetooth pin setting */
+    //TX
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    
+	//RX
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
 }
 
-void EXTI_Configure(void) // stm32f10x_gpio.h 참고
+
+void USART_Init(void)
 {
-    EXTI_InitTypeDef EXTI_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
 
-	// TODO: Select the GPIO pin (Joystick, button) used as EXTI Line using function 'GPIO_EXTILineConfig'
-	// TODO: Initialize the EXTI using the structure 'EXTI_InitTypeDef' and the function 'EXTI_Init'
-	
-    /* Joystick Down */
-	GPIO_EXTILineConfig(GPIO_PortSourceGPIOC, GPIO_PinSource2);
-    EXTI_InitStructure.EXTI_Line = EXTI_Line2;
-    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
-    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
-    EXTI_Init(&EXTI_InitStructure);
-
-    /* Joystick Up */
-	GPIO_EXTILineConfig(GPIO_PortSourceGPIOC, GPIO_PinSource5);
-    EXTI_InitStructure.EXTI_Line = EXTI_Line5;
-    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
-    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
-    EXTI_Init(&EXTI_InitStructure);
-
-    /* Button */
-    GPIO_EXTILineConfig(GPIO_PortSourceGPIOD, GPIO_PinSource11);
-    EXTI_InitStructure.EXTI_Line = EXTI_Line11;
-    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
-    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
-    EXTI_Init(&EXTI_InitStructure);
-	
-	// NOTE: do not select the UART GPIO pin used as EXTI Line here
-}
-
-void USART1_Init(void) // stm32f10x_usart.h 참고
-{
-	USART_InitTypeDef USART1_InitStructure;
-
-	// Enable the USART1 peripheral
+	// Enable the USART1, USART2 peripheral
 	USART_Cmd(USART1, ENABLE);
+    USART_Cmd(USART2, ENABLE);
 	
-	// TODO: Initialize the USART using the structure 'USART_InitTypeDef' and the function 'USART_Init'
 	/* 
        BaudRate: 9600
        WordLength: 8bits
@@ -132,16 +87,19 @@ void USART1_Init(void) // stm32f10x_usart.h 참고
        StopBits: 1bit
        Hardware Flow Control: None
      */
-    USART1_InitStructure.USART_BaudRate = 9600;
-    USART1_InitStructure.USART_WordLength = (uint16_t) USART_WordLength_8b;
-    USART1_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-    USART1_InitStructure.USART_Parity = (uint16_t) USART_Parity_No;
-    USART1_InitStructure.USART_StopBits = (uint16_t) USART_StopBits_1;
-    USART1_InitStructure.USART_HardwareFlowControl = (uint16_t) USART_HardwareFlowControl_None;
-    USART_Init(USART1, &USART1_InitStructure);
+    USART_InitStructure.USART_BaudRate = 9600;
+    USART_InitStructure.USART_WordLength = (uint16_t) USART_WordLength_8b;
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+    USART_InitStructure.USART_Parity = (uint16_t) USART_Parity_No;
+    USART_InitStructure.USART_StopBits = (uint16_t) USART_StopBits_1;
+    USART_InitStructure.USART_HardwareFlowControl = (uint16_t) USART_HardwareFlowControl_None;
+
+    USART_Init(USART1, &USART_InitStructure);
+    USART_Init(USART2, &USART_InitStructure);
     
-	// TODO: Enable the USART1 RX interrupts using the function 'USART_ITConfig' and the argument value 'Receive Data register not empty interrupt'
+
     USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+    USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
 	
 }
 
@@ -150,30 +108,9 @@ void NVIC_Configure(void) { // misc.h 참고
     NVIC_InitTypeDef NVIC_InitStructure;
     
     // TODO: fill the arg you want
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
 
 	// TODO: Initialize the NVIC using the structure 'NVIC_InitTypeDef' and the function 'NVIC_Init'
-	
-    // Joystick Down
-    NVIC_InitStructure.NVIC_IRQChannel = EXTI2_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x1; // TODO
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0; // TODO
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_Init(&NVIC_InitStructure);
-
-    // Joystick Up
-    NVIC_InitStructure.NVIC_IRQChannel = EXTI9_5_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x2; // TODO
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0; // TODO
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_Init(&NVIC_InitStructure);
-    
-    // User S1 Button
-    NVIC_InitStructure.NVIC_IRQChannel = EXTI15_10_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x3; // TODO
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0; // TODO
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_Init(&NVIC_InitStructure);
 
     // UART1
 	// 'NVIC_EnableIRQ' is only required for USART setting
@@ -183,67 +120,36 @@ void NVIC_Configure(void) { // misc.h 참고
     NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0; // TODO
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&NVIC_InitStructure);
+
+    // UART2
+    NVIC_EnableIRQ(USART2_IRQn);
+    NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x1; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
 }
 
 void USART1_IRQHandler() {
-	uint16_t word;
     if(USART_GetITStatus(USART1,USART_IT_RXNE)!=RESET){
-    	// the most recent received data by the USART1 peripheral
-        word = USART_ReceiveData(USART1);
-
-        // TODO implement, 키보드 입력 'd' 또는 'u'에 따라 동작
-        switch (word)
-        {
-        case 'u':
-            sw_state = (sw_state & 0x02) | (0x00);
-            break;
-        case 'd':
-            sw_state = (sw_state & 0x02) | (0x01);
-            break;
-        default:
-            break;
-        }
-
+    	
+        flagPC = 1;
+        dataBufferFromPC = USART_ReceiveData(USART1);
+        
         // clear 'Read data register not empty' flag
     	USART_ClearITPendingBit(USART1,USART_IT_RXNE);
     }
 }
 
-void EXTI15_10_IRQHandler(void) { // when the button is pressed
-
-	if (EXTI_GetITStatus(EXTI_Line11) != RESET) {
-          for(int i=0;i<100000;i++) {}
-		if (GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_11) == Bit_RESET) {
-                  sw_state = (0x02) | (sw_state & 0x01);
-		} 
-        EXTI_ClearITPendingBit(EXTI_Line11);
-	}
+void USART2_IRQHandler() {
+    if(USART_GetITStatus(USART1,USART_IT_RXNE)!=RESET){
+    	
+        flagBT = 1;
+        dataBufferFromBT = USART_ReceiveData(USART2);
+        
+    	USART_ClearITPendingBit(USART2,USART_IT_RXNE);
+    }
 }
-
-
-// TODO: Create Joystick interrupt handler functions
-/* Use "EXTIx_IRQHandler()" */
-// Joystic 'Up'
-void EXTI9_5_IRQHandler(void) {
-    if (EXTI_GetITStatus(EXTI_Line5) != RESET) {
-		if (GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_5) == Bit_RESET) {
-            sw_state = (sw_state & 0x02) | (0x00);
-		}
-        EXTI_ClearITPendingBit(EXTI_Line5);
-	}
-}
-
-// Joystic 'Down'
-void EXTI2_IRQHandler(void) {
-    if (EXTI_GetITStatus(EXTI_Line2) != RESET) {
-		if (GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_2) == Bit_RESET) {
-            sw_state = (sw_state & 0x02) | (0x01);
-			
-		}
-        EXTI_ClearITPendingBit(EXTI_Line2);
-	}
-}
-
 
 void Delay(void) {
 	int i;
@@ -253,15 +159,16 @@ void Delay(void) {
 
 void sendDataUART1(uint16_t data) {
 	USART_SendData(USART1, data);
-	/* Wait till TC is set */
-	while ((USART1->SR & USART_SR_TC) == 0);
+}
+
+void sendDataUART2(uint16_t data) {
+	USART_SendData(USART2, data);
 }
 
 int main(void)
 {   
     uint16_t ledPin[] = {GPIO_Pin_2, GPIO_Pin_3, GPIO_Pin_4, GPIO_Pin_7}; 
     unsigned int ledIdx = 3;
-    char msg[] = "TEAM09\r\n";
 
     sw_state = 0x00;
 
@@ -273,32 +180,20 @@ int main(void)
 
     EXTI_Configure();
 
-    USART1_Init();
+    USART_Init();
 
     NVIC_Configure();
 
     while (1) {
     	// TODO: implement
 
-    	if((sw_state & 0x02) == 0x02) {
-            char* pmsg = msg;
-            while(*pmsg != 0) {
-                sendDataUART1(*pmsg);
-                pmsg++;
-            }
-
-            sw_state = (sw_state & 0x01);
+    	if(flagPC == 1) {
+            sendDataUART2(dataBufferFromPC);
+            flagPC = 0;
+        } else if (flagBT == 1) {
+            sendDataUART1(dataBufferFromBT);
+            flagBT = 0;
         }
-    	
-        if((sw_state & 0x01) == 0x00) {
-            ledIdx++;
-            ledIdx %= 4;
-        } else if((sw_state & 0x01) == 0x01) {
-            ledIdx--;
-            ledIdx %= 4;
-        }
-        GPIO_Write(GPIOD, ledPin[ledIdx]);
-        Delay();
     }
     return 0;
 }
---
+++ Week09/main.c
@@ -0,0 +1,199 @@
+#include "stm32f10x.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_usart.h"
+#include "stm32f10x_rcc.h"
+
+#include "misc.h"
+
+/* function prototype */
+void RCC_Configure(void);
+void GPIO_Configure(void);
+void EXTI_Configure(void);
+void USART_Init(void);
+void NVIC_Configure(void);
+
+void EXTI15_10_IRQHandler(void);
+void EXTI9_5_IRQHandler(void);
+void EXTI2_IRQHandler(void);
+
+void Delay(void);
+
+void sendDataUART1(uint16_t data);
+void sendDataUART2(uint16_t data);
+
+char dataBufferFromPC;
+char flagPC = 0;
+
+char dataBufferFromBT;
+char flagBT = 0;
+
+
+//---------------------------------------------------------------------------------------------------
+
+void RCC_Configure(void) // stm32f10x_rcc.h 참고
+{
+	/* USART1 clock enable */
+    RCC_APB2PeriphClockCmd(RCC_APB2ENR_USART1EN, ENABLE);
+
+	/* USART2 clock enable */
+    RCC_APB2PeriphClockCmd(RCC_APB2ENR_USART2EN, ENABLE);
+}
+
+void GPIO_Configure(void) // stm32f10x_gpio.h 참고
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+
+
+    /* UART between PC pin setting */
+    //TX
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    
+	//RX
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+    /* UART between bluetooth pin setting */
+    //TX
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    
+	//RX
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+}
+
+
+void USART_Init(void)
+{
+	USART_InitTypeDef USART_InitStructure;
+
+	// Enable the USART1, USART2 peripheral
+	USART_Cmd(USART1, ENABLE);
+    USART_Cmd(USART2, ENABLE);
+	
+	/* 
+       BaudRate: 9600
+       WordLength: 8bits
+       Parity: None
+       StopBits: 1bit
+       Hardware Flow Control: None
+     */
+    USART_InitStructure.USART_BaudRate = 9600;
+    USART_InitStructure.USART_WordLength = (uint16_t) USART_WordLength_8b;
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+    USART_InitStructure.USART_Parity = (uint16_t) USART_Parity_No;
+    USART_InitStructure.USART_StopBits = (uint16_t) USART_StopBits_1;
+    USART_InitStructure.USART_HardwareFlowControl = (uint16_t) USART_HardwareFlowControl_None;
+
+    USART_Init(USART1, &USART_InitStructure);
+    USART_Init(USART2, &USART_InitStructure);
+    
+
+    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+    USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
+	
+}
+
+void NVIC_Configure(void) { // misc.h 참고
+
+    NVIC_InitTypeDef NVIC_InitStructure;
+    
+    // TODO: fill the arg you want
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+
+	// TODO: Initialize the NVIC using the structure 'NVIC_InitTypeDef' and the function 'NVIC_Init'
+
+    // UART1
+	// 'NVIC_EnableIRQ' is only required for USART setting
+    NVIC_EnableIRQ(USART1_IRQn);
+    NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x0; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    // UART2
+    NVIC_EnableIRQ(USART2_IRQn);
+    NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x1; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+}
+
+void USART1_IRQHandler() {
+    if(USART_GetITStatus(USART1,USART_IT_RXNE)!=RESET){
+    	
+        flagPC = 1;
+        dataBufferFromPC = USART_ReceiveData(USART1);
+        
+        // clear 'Read data register not empty' flag
+    	USART_ClearITPendingBit(USART1,USART_IT_RXNE);
+    }
+}
+
+void USART2_IRQHandler() {
+    if(USART_GetITStatus(USART1,USART_IT_RXNE)!=RESET){
+    	
+        flagBT = 1;
+        dataBufferFromBT = USART_ReceiveData(USART2);
+        
+    	USART_ClearITPendingBit(USART2,USART_IT_RXNE);
+    }
+}
+
+void Delay(void) {
+	int i;
+
+	for (i = 0; i < 2000000; i++) {}
+}
+
+void sendDataUART1(uint16_t data) {
+	USART_SendData(USART1, data);
+}
+
+void sendDataUART2(uint16_t data) {
+	USART_SendData(USART2, data);
+}
+
+int main(void)
+{   
+    uint16_t ledPin[] = {GPIO_Pin_2, GPIO_Pin_3, GPIO_Pin_4, GPIO_Pin_7}; 
+    unsigned int ledIdx = 3;
+
+    sw_state = 0x00;
+
+    SystemInit();
+
+    RCC_Configure();
+
+    GPIO_Configure();
+
+    EXTI_Configure();
+
+    USART_Init();
+
+    NVIC_Configure();
+
+    while (1) {
+    	// TODO: implement
+
+    	if(flagPC == 1) {
+            sendDataUART2(dataBufferFromPC);
+            flagPC = 0;
+        } else if (flagBT == 1) {
+            sendDataUART1(dataBufferFromBT);
+            flagBT = 0;
+        }
+    }
+    return 0;
+}
