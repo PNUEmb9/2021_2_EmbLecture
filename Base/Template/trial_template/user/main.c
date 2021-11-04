10e375d7c8d8c8a317ad2f2e612d9af848211026
JaeHeyK <jaeheyk@gmail.com>
2021-11-04 13:32:58 +0900

    [Week10] Update ADC configure code

--- Base/Template/trial_template/user/main.c
+++ Base/Template/trial_template/user/main.c
@@ -12,7 +12,7 @@
 /* function prototype */
 void RCC_Configure(void);
 void GPIO_Configure(void);
-void EXTI_Configure(void);
+void ADC_Configure(void);
 void USARTS_Init(void);
 void NVIC_Configure(void);
 
@@ -32,7 +32,8 @@ uint16_t lumiValue;
 
 void RCC_Configure(void) // stm32f10x_rcc.h 참고
 {
-    
+    // ADC! pin clock enable
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
 	/* USART pin clock enable */
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 
@@ -49,6 +50,10 @@ void RCC_Configure(void) // stm32f10x_rcc.h 참고
 void GPIO_Configure(void) // stm32f10x_gpio.h 참고
 {
     GPIO_InitTypeDef GPIO_InitStructure;
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
 
     /* UART between PC pin setting */
     //TX
@@ -74,6 +79,31 @@ void GPIO_Configure(void) // stm32f10x_gpio.h 참고
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 	
+}
+
+void ADC_Configure(void) {
+    ADC_InitTypeDef ADC_InitStructure;
+    ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+    ADC_InitStructure.ADC_ScanConvMode = DISABLE;
+    ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
+    ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+    ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+    ADC_InitStructure.ADC_NbrOfChannel = 1;
+
+    ADC_Init(ADC1, &ADC_InitStructure);
+
+    ADC_RegularChannelConfig(ADC1,ADC_Channel_0,1,ADC_SampleTime_55Cycles5);
+    ADC_ITConfig(ADC1,ADC_IT_EOC,ENABLE);
+    ADC_Cmd(ADC1,ENABLE);
+
+    ADC_ResetCalibration(ADC1);
+    while (ADC_GetResetCalibrationStatus) {}
+    
+    ADC_StartCalibration(ADC1);
+    while(ADC_GetCalibrationStatus) {}
+
+    ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+    
 }
 
 
@@ -127,6 +157,10 @@ void NVIC_Configure(void) { // misc.h 참고
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x1; 
     NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0;
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    
+    //ADC1
+    NVIC_EnableIRQ(ADC1_2_IRQn);
+    
     NVIC_Init(&NVIC_InitStructure);
 }
 
@@ -145,6 +179,13 @@ void USART2_IRQHandler() {
         dataBufferFromBT = USART_ReceiveData(USART2);
         
     	USART_ClearITPendingBit(USART2,USART_IT_RXNE);
+    }
+}
+
+void ADC1_2_IRQHandler() {
+    if(ADC_GetITStatus(ADC1,ADC_IT_EOC)!=RESET) {
+        lumiValue = ADC_GetConversionValue(ADC1);
+        ADC_ClearITPendingBit(ADC1, ADC_IT_EOC);
     }
 }
 
